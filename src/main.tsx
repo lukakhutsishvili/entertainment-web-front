a9e3031071dfda36a0be85c8eb30c9eeb386e6b5
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-05-03 22:50:26 +0400

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,10 @@ import store from "./store/redux.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+      <BrowserRouter>
       <Provider store={store}>
         <App />
-      </Provider>
-    </BrowserRouter>
+        </Provider>
+      </BrowserRouter>
   </React.StrictMode>
 );
